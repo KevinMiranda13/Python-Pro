4281b625ce8b5da2b881e513a0b6641594758336
KevinMiranda13 <kevmiranda1110@gmail.com>
2025-05-29 19:03:09 -0600

    primer archivo

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+caracters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+longitud = int(input("Ingrese la longitud de su contraseña: "))
+password = ""
+for i in range(longitud):
+    password += random.choice(caracters)
+    
+print(password)
